2e6055489e32ca297cb7b35a4778a8ab29364b0e
moof44 <jireh.padua@gmail.com>
2024-08-08 20:37:24 +0800

    Added angular/fire

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -4,10 +4,16 @@ import { provideRouter } from '@angular/router';
 import { routes } from './app.routes';
 import { provideClientHydration } from '@angular/platform-browser';
 import { provideServiceWorker } from '@angular/service-worker';
+import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
+import { getAuth, provideAuth } from '@angular/fire/auth';
+import { getFirestore, provideFirestore } from '@angular/fire/firestore';
+import { getFunctions, provideFunctions } from '@angular/fire/functions';
+import { getStorage, provideStorage } from '@angular/fire/storage';
 
 export const appConfig: ApplicationConfig = {
   providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes), provideClientHydration(), provideServiceWorker('ngsw-worker.js', {
             enabled: !isDevMode(),
             registrationStrategy: 'registerWhenStable:30000'
-          })]
+  //        }), provideFirebaseApp(() => initializeApp({"projectId":"personal-accounting-3fbcb","appId":"1:1007338909314:web:1846f93f43e5eb550c4e9b","storageBucket":"personal-accounting-3fbcb.appspot.com","locationId":"asia-southeast1","apiKey":"AIzaSyAB10hD2BBTuyiBIuEwco8XbzWSmUn_jrw","authDomain":"personal-accounting-3fbcb.firebaseapp.com","messagingSenderId":"1007338909314","measurementId":"G-L0E42WYYJY"})), provideAuth(() => getAuth()), provideFirestore(() => getFirestore()), provideFunctions(() => getFunctions()), provideStorage(() => getStorage())]
+          }), provideFirebaseApp(() => initializeApp({"projectId":"personal-accounting-3fbcb","appId":"1:1007338909314:web:1846f93f43e5eb550c4e9b","storageBucket":"personal-accounting-3fbcb.appspot.com","apiKey":"AIzaSyAB10hD2BBTuyiBIuEwco8XbzWSmUn_jrw","authDomain":"personal-accounting-3fbcb.firebaseapp.com","messagingSenderId":"1007338909314","measurementId":"G-L0E42WYYJY"})), provideAuth(() => getAuth()), provideFirestore(() => getFirestore()), provideFunctions(() => getFunctions()), provideStorage(() => getStorage())]
 };
