b084dccc7f5dd414a6f3fffeb0815181302fb1c4
moof44 <moof44@yahoo.com>
2024-08-16 09:05:34 +0800

    Update environment.ts

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,11 +1,1 @@
-export const environment = {
-    firebaseConfig:{
-        projectId: 'personal-accounting-3fbcb',
-        appId: '1:1007338909314:web:1846f93f43e5eb550c4e9b',
-        storageBucket: 'personal-accounting-3fbcb.appspot.com',
-        apiKey: 'AIzaSyAB10hD2BBTuyiBIuEwco8XbzWSmUn_jrw',
-        authDomain: 'personal-accounting-3fbcb.firebaseapp.com',
-        messagingSenderId: '1007338909314',
-        measurementId: 'G-L0E42WYYJY',
-      }
-};
+export const environment = {};
