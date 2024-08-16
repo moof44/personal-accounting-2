c1db20533ca66b29ba848fb7132a6576a942cede
moof44 <jireh.padua@gmail.com>
2024-08-16 09:03:44 +0800

    Main - modified environment

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -18,6 +18,7 @@ import { provideStore } from '@ngrx/store';
 import { provideEffects } from '@ngrx/effects';
 import { provideRouterStore } from '@ngrx/router-store';
 import { withComponentInputBinding } from '@angular/router';
+import { environment } from 'environments/environment.development';
 
 export const appConfig: ApplicationConfig = {
   providers: [
@@ -30,15 +31,7 @@ export const appConfig: ApplicationConfig = {
       //        }), provideFirebaseApp(() => initializeApp({"projectId":"personal-accounting-3fbcb","appId":"1:1007338909314:web:1846f93f43e5eb550c4e9b","storageBucket":"personal-accounting-3fbcb.appspot.com","locationId":"asia-southeast1","apiKey":"AIzaSyAB10hD2BBTuyiBIuEwco8XbzWSmUn_jrw","authDomain":"personal-accounting-3fbcb.firebaseapp.com","messagingSenderId":"1007338909314","measurementId":"G-L0E42WYYJY"})), provideAuth(() => getAuth()), provideFirestore(() => getFirestore()), provideFunctions(() => getFunctions()), provideStorage(() => getStorage())]
     }),
     provideFirebaseApp(() =>
-      initializeApp({
-        projectId: 'personal-accounting-3fbcb',
-        appId: '1:1007338909314:web:1846f93f43e5eb550c4e9b',
-        storageBucket: 'personal-accounting-3fbcb.appspot.com',
-        apiKey: 'AIzaSyAB10hD2BBTuyiBIuEwco8XbzWSmUn_jrw',
-        authDomain: 'personal-accounting-3fbcb.firebaseapp.com',
-        messagingSenderId: '1007338909314',
-        measurementId: 'G-L0E42WYYJY',
-      })
+      initializeApp(environment.firebaseConfig)
     ),
     provideAuth(() => getAuth()),
     provideFirestore(() => getFirestore()),
