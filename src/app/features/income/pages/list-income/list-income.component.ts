7b437032334be90b36da24eedc8c85f5f5060c1d
moof44 <moof44@yahoo.com>
2024-08-21 23:49:12 +0800

    P1 - added search in income

--- src/app/features/income/pages/list-income/list-income.component.ts
+++ src/app/features/income/pages/list-income/list-income.component.ts
@@ -1,11 +1,15 @@
 import { CommonModule } from '@angular/common';
 import { ChangeDetectionStrategy, Component, HostBinding, inject, input, signal, type OnInit } from '@angular/core';
+import { FormBuilder, FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { MatButtonModule } from '@angular/material/button';
 import { MatCardModule } from '@angular/material/card';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
 import { MatTableModule } from '@angular/material/table';
 import { Router } from '@angular/router';
 import { Income } from '@app/models/global.model';
 import { IncomeStore } from '@app/shared/store/income.store';
+import { debounceTime, distinctUntilChanged } from 'rxjs';
 
 @Component({
   selector: 'page-list-income',
@@ -15,6 +19,10 @@ import { IncomeStore } from '@app/shared/store/income.store';
     MatTableModule,
     MatCardModule,
     MatButtonModule,
+    MatInputModule,
+    MatFormFieldModule,
+    FormsModule,
+    ReactiveFormsModule,
   ],
   templateUrl: './list-income.component.html',
   styleUrl: './list-income.component.scss',
@@ -23,13 +31,27 @@ import { IncomeStore } from '@app/shared/store/income.store';
 export class ListIncomeComponent implements OnInit {
   readonly store = inject(IncomeStore);
   private _router = inject(Router);
+  private _fb = inject(FormBuilder);
 
+  search = this._fb.control('');
 
-  displayedColumns: string[] = ['date', 'incomeSource', 'amount', 'remarks'];
-  dataSource = this.store.entities; // dataSource = input.required<Income[]>() // in case this will be a separate component
+  displayedColumns: string[] = ['date', 'incomeSource', 'amount'/* , 'remarks' */];
+  dataSource = this.store.filteredEntities; // dataSource = input.required<Income[]>() // in case this will be a separate component
 
   ngOnInit(): void {
     //this.dataSource.set(this.store.entities());
+    this.#_eventListener();
+  }
+
+  #_eventListener(){
+    this.search.valueChanges
+      .pipe(
+        debounceTime(300),
+        distinctUntilChanged(),
+      )
+      .subscribe(v=>{
+        this.store.setFilter(v??'', 'asc');
+      })
   }
 
   onRowClick(id: number) {
