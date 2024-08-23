b3291b1b58d726c12172170525acb29ae21d7c95
moof44 <moof44@yahoo.com>
2024-08-24 00:49:44 +0800

    P1 - completed about page

--- src/app/features/about/about.component.ts
+++ src/app/features/about/about.component.ts
@@ -1,17 +1,20 @@
 import { CommonModule } from '@angular/common';
-import { ChangeDetectionStrategy, Component, type OnInit } from '@angular/core';
+import { ChangeDetectionStrategy, Component, HostBinding, type OnInit } from '@angular/core';
+import { MatCardModule } from '@angular/material/card';
 
 @Component({
   selector: 'feature-about',
   standalone: true,
   imports: [
     CommonModule,
+    MatCardModule,
   ],
   templateUrl: './about.component.html',
   styleUrl: './about.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class AboutComponent implements OnInit {
+  @HostBinding('class.content__page') pageClass = true;
 
   ngOnInit(): void { }
 
